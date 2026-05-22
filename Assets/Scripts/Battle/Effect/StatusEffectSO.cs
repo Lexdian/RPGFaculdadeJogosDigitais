8786d09c4a63c5ca106e9858419a863a2ba8573d
Lucas Ferruccio <lucaspaiva.ufrpe@gmail.com>
2026-05-21 23:01:08 -0300

    Adicionando as estruturas bases do StatusEffect

---
+++ Assets/Scripts/Battle/Effect/StatusEffectSO.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public enum TipoDanoStatus { Nenhum, Normal, Fogo, Gelo, Eletrico, Sombrio, Luz, Agua, Vento, Terra }
+
+public enum DuracaoStatus { Combate, Persistente };
+
+[CreateAssetMenu(fileName = "StatusEffectSO", menuName = "RPG/StatusEffect")]
+public abstract class StatusEffectSO : ScriptableObject
+{
+    [Header("Identidade")]
+    public string effectName;
+    [TextArea(2,3)] public string effectDescription;
+    public Sprite icon;
+
+    [Header("Regras")]
+    public DuracaoStatus duracaoStatus;
+    public TipoDanoStatus tipoDano;
+    public int valorDano;
+    public int duracao;
+
+    [Header("Efeitos Visuais e Sonoros")]
+    public string gatilhoAnimacao = "StatusEffect";
+    public GameObject efeitoVisual;
+
+    public abstract void OnApply(BattleEntity target, BattleEntity source);
+
+    public abstract void OnTick(BattleEntity target, StatusEffectInstance instance);
+
+    public abstract void OnExpire(BattleEntity target);
+
+    public virtual bool IsStackable => false;
+}
